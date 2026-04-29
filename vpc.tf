bf87d7f1af420596c8c1d79b87935024d7b4f368
Herrera <Tony.Herrera@gdit.com>
2026-04-29 16:29:25 -0700

    adding suffix to route table gateways

--- vpc.tf
+++ vpc.tf
@@ -62,7 +62,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.watchdog_v4.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw
+    gateway_id = aws_internet_gateway.igw.id  # Added .id
   }
   tags = { Name = "watchdog-public-rt" }
 }
@@ -82,7 +82,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.watchdog_v4.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat
+    nat_gateway_id = aws_nat_gateway.nat.id  # Added .id
   }
   tags = { Name = "watchdog-private-rt" }
 }
