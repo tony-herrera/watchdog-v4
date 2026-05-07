d2e83ba1421dffc4e50f3509226e3fb939723a94
Herrera <Tony.Herrera@gdit.com>
2026-05-06 20:54:50 -0700

    adding dynamo db storage

---
+++ database.tf
@@ -0,0 +1,18 @@
+# 1. The Price Sentinel Table
+# This stores current prices and the "Freshness" thresholds for our cache.
+resource "aws_dynamodb_table" "price_sentinel" {
+  name         = "watchdog-price-sentinel"
+  billing_mode = "PAY_PER_REQUEST" # Pay only for what users search
+  hash_key     = "Ticker"         # Our "Partition Key" (the unique ID)
+
+  attribute {
+    name = "Ticker"
+    type = "S" # S stands for String (e.g., "NVDA")
+  }
+
+  # This allows the table to be reachable via our private VPC Endpoint
+  tags = {
+    Name = "watchdog-price-sentinel"
+    Environment = "production"
+  }
+}
